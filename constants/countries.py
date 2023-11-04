b029096489bf25cee91cf9a2fd5642ff8de0fff4
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-11-04 12:30:25 -0600

    end of story

--- constants/countries.py
+++ constants/countries.py
@@ -90,29 +90,35 @@ AFRICA = ['Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', '
           'Guinea-Bissau', 'Ivory Coast', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali',
           'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda',
           'Sao Tome and Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan',
-          'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe']
+          'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe', "Cape Verde", "Cote d'Ivoire",
+          "Western Sahara","Swaziland","Saint Helena","Reunion","Mayotte","Democratic Republic of Congo", "Kenya"]
 
 ASIA = ['Afghanistan', 'Armenia', 'Azerbaijan', 'Bahrain', 'Bangladesh', 'Bhutan', 'Brunei', 'Cambodia', 'China',
         'Cyprus', 'Georgia', 'India', 'Indonesia', 'Iran', 'Iraq', 'Israel', 'Japan', 'Jordan', 'Kazakhstan',
         'Kuwait', 'Kyrgyzstan', 'Laos', 'Lebanon', 'Malaysia', 'Maldives', 'Mongolia', 'Myanmar', 'Nepal',
         'North Korea', 'Oman', 'Pakistan', 'Palestine', 'Philippines', 'Qatar', 'Saudi Arabia', 'Singapore',
         'South Korea', 'Sri Lanka', 'Syria', 'Taiwan', 'Tajikistan', 'Thailand', 'Timor-Leste', 'Turkey', 'Turkmenistan',
-        'United Arab Emirates', 'Uzbekistan', 'Vietnam', 'Yemen']
+        'United Arab Emirates', 'Uzbekistan', 'Vietnam', 'Yemen',
+        "Faroe Islands","Hong Kong", "Macao"]
 
 EUROPE = ['Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia',
           'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
           'Iceland', 'Ireland', 'Italy', 'Kosovo', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta',
           'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland', 'Portugal',
           'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland',
-          'Ukraine', 'United Kingdom', 'Vatican City']
+          'Ukraine', 'United Kingdom', 'Vatican City', "Czechia",
+          "Serbia and Montenegro","Gibraltar", "Latvia"]
 
 NORTH_AMERICA = ['Antigua and Barbuda', 'Bahamas', 'Barbados', 'Belize', 'Canada', 'Costa Rica', 'Cuba', 'Dominica',
                  'Dominican Republic', 'El Salvador', 'Grenada', 'Guatemala', 'Haiti', 'Honduras', 'Jamaica', 'Mexico',
                  'Nicaragua', 'Panama', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines',
-                 'Trinidad and Tobago', 'United States']
+                 'Trinidad and Tobago', 'United States', 'Anguilla', "Bermuda", "Cayman Islands",
+                 "United States Virgin Islands","Turks and Caicos Islands","Saint Pierre and Miquelon","Puerto Rico","Netherlands Antilles","Montserrat","Greenland", "Guadeloupe", "Martinique"]
 
 SOUTH_AMERICA = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru',
-                 'Suriname', 'Uruguay', 'Venezuela']
+                 'Suriname', 'Uruguay', 'Venezuela', 'Aruba',
+                 "Falkland Islands","French Guiana"]
 
 OCEANIA = ['Australia', 'Fiji', 'Kiribati', 'Marshall Islands', 'Nauru', 'New Zealand', 'Palau',
-           'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga', 'Tuvalu', 'Vanuatu']
+           'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga', 'Tuvalu', 'Vanuatu', "American Samoa", "British Virgin Islands",
+           "Wallis and Futuna","Tokelau","Northern Mariana Islands","Niue","New Caledonia","Cook Islands", "French Polynesia","Guam", "Micronesia"]
